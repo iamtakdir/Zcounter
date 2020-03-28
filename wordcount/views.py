5a3dcb54e7d12ecf74d83e19ff4fc263526d57a7
iamtakdir <takdirofficial@gmail.com>
2020-03-29 05:21:24 +0600

    yo

---
+++ wordcount/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def home (request):
+
+    return render(request,'home.html')
+
+def count(request):
+    fulltext= request.GET['fulltext']
+
+    wordlist= fulltext.split()
+
+    worddic ={}
+
+    for word in wordlist:
+
+        if word in worddic:
+            #increase
+            worddic[word]+=1
+        else:
+            #add to dic 
+            worddic[word]=1
+    sortedwords=sorted(worddic.items(),key=operator.itemgetter(1),reverse=True)
+
+
+    return render(request,'count.html',{'fulltext':fulltext,'count':len(wordlist),'sortedwords':sortedwords})
